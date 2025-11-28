4bc6a8b101b93a25b63802381f9e16526aa1795e
MosRamTest <mosram2@gmail.com>
2025-11-28 19:16:40 +0200

    first commit

---
+++ Pages/__init__.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.ndosiautomation.co.za/")
+
+driver.find_element(By.ID, "overview-hero").is_displayed()
+
+
+Welcome_text = driver.find_element(By.XPATH, '//*[@id="overview-hero"]/h2').text
+
+if Welcome_text == "Learn Automation the Right Way":
+    print("Test Passed: Welcome text is correct.")
+    assert True
+else:
+    print("Test Failed: Welcome text is incorrect.")
+    assert False
+
+driver.find_element(By.ID, "nav-btn-practice").click()
+driver.find_element(By.ID, "login-email").send_keys("king@gmail.com")
+driver.find_element(By.ID, "login-password").send_keys("King1234")
+driver.find_element(By.ID, "login-submit").click()
+
+input("Press ENTER to close...")
---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Project
+
+* This a simple Python Project template.
+
+## Software Requirements
+* Python 3.8 or higher
+* pip (Python package installer)
+* virtualenv (optional but recommended)
+* Git (for version control)
+* An IDE or text editor (e.g., VSCode, PyCharm, Sublime Text)
+* GitHub account (for repository hosting)
